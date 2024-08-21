e9526f91d2fcd8b1d59005053c38b408118af356
noway4you <noway2266@gmail.com>
2024-08-21 11:56:31 +0800

    2024/8/21

---
+++ testWeb/src/main/java/tw/test/apis/MyUtils.java
@@ -0,0 +1,19 @@
+package tw.test.apis;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class MyUtils {
+	public static String loadView() throws Exception {
+		String url = "D:/JavaPractice/testWeb/src/main/webapp/views/view1.html";
+		String source = url;
+		File viewFile = new File(source);
+		int length = (int)viewFile.length();
+		byte[] buffer = new byte[length];
+		BufferedInputStream bin = new BufferedInputStream(new FileInputStream(viewFile));
+		bin.read(buffer);
+		bin.close();
+		return new String(buffer);
+	}
+}
